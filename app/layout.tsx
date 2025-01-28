74c222a705eab530c91a2734238264432b7f4a35
Cole Ogden <coleogden@datasmithva.com>
2025-01-27 19:23:46 -0500

    Initial setup and under maintenence component created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./ui/globals.css";
 
+// Components
+import UnderMaint from "./ui/under_maint/under_maint";
 
 export const metadata: Metadata = {
-  title: "DataSmith",
-  description: "webapp for DataSmith",
+  title: {
+    template: "DataSmith | %s",
+    default: "DataSmith | Home",
+  },
+  description: "DataSmith LLC",
 };
 
 export default function RootLayout({
@@ -15,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <UnderMaint />
         {children}
       </body>
     </html>
