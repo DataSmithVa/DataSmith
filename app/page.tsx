680b0a6ac8d994a1ded258201759c80d1fe6c584
Cole Ogden <coleogden@datasmithva.com>
2025-02-03 01:21:45 -0500

    Disabled dev components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import UnderMaint from "./ui/under_maint";
 
 // DEV
-import PunchClock from "./ui/punch_clock";
+
 
 export default function Home() {
   return (
     <div>
-      <PunchClock />
-      <UnderMaint />
+            <UnderMaint />
     </div>
   );
 }
--- app/ui/under_maint.tsx
+++ app/ui/under_maint.tsx
@@ -6,7 +6,7 @@ export default function UnderMaint() {
     <main className="grid h-screen place-items-center">
       <div className="grid m-auto place-items-center w-3/4 md:w-1/4 p-10 bg-slate-300 rounded-md text-center drop-shadow-xl">
         <Image src={logo} alt="Logo" width={500} height={500}  className="w-3/4"/>
-        <h1 className=" text-red-500 p-4 text-md"> Site is currently under maintenence</h1>
+        <h1 className=" font-medium text-red-500 p-4 text-md"> Site is currently under maintenence</h1>
         <h3 className="text-sm text-slate-500">If you have any questions please reach out to</h3>
         <h3 className=" font-semibold p-2 text-slate-900">inquiries@datasmithva.com</h3>
       </div>
